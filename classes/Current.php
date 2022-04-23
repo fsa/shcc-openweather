e8d3a6345ec667fa6eaea79535013061e3e771ef
Sergei Fedotov <fsa@tavda.info>
2022-04-24 02:18:25 +0700

    Удаление зависимости от других пакетов. Форматирование кода. Добавление событий вместо лишних зависимостей.

--- classes/Current.php
+++ classes/Current.php
@@ -2,155 +2,193 @@
 
 namespace FSA\OpenWeatherPlugin;
 
-use FSA\OpenWeatherMap\Api,
-    SmartHome\Devices,
-    SmartHome\DeviceStorage,
-    SmartHome\DeviceInterface;
+use SmartHome\DeviceInterface;
 
-class Current implements DeviceInterface {
+class Current implements DeviceInterface
+{
+    const OWM_URL = 'https://api.openweathermap.org/data/2.5/';
 
-    public $weather;
-
-    /** @var OpenWeatherMap\Api * */
-    private $owm;
     private $hwid;
     private $api_key;
     private $city_id;
+    private $weather;
+    private $events;
     private $updated;
 
-    public function __construct() {
-        $this->updated=0;
+    public function __construct()
+    {
+        $this->updated = 0;
     }
 
-    public function getEventsList(): array {
+    public function getEventsList(): array
+    {
         return ['temperature', 'humidity', 'pressure', 'wind_speed', 'wind_direction'];
     }
 
-    public function getDescription(): string {
+    public function getDescription(): string
+    {
         return 'Текущая погода с OpenWeatherMap.org';
     }
 
-    public function getHwid(): string {
+    public function getHwid(): string
+    {
         return $this->hwid;
     }
 
-    public function getState(): array {
-        if(is_null($this->weather)) {
+    public function getState(): array
+    {
+        if (is_null($this->weather)) {
             return [];
         }
         return [
-            'temperature'=>round($this->getTemperature(),1),
-            'temp_feels_like'=>round($this->getTempFeelsLike(),1),
-            'humidity'=>round($this->getHumidity()),
-            'pressure'=>$this->getPressure(),
-            'description'=>$this->weather->weather[0]->description,
-            'wind_speed'=>$this->weather->wind->speed,
-            'wind_direction'=>$this->weather->wind->deg,
-            'wind_direction_string'=>$this->getWindDirection()
-                ];    }
+            'temperature' => round($this->getTemperature(), 1),
+            'temp_feels_like' => round($this->getTempFeelsLike(), 1),
+            'humidity' => round($this->getHumidity()),
+            'pressure' => $this->getPressure(),
+            'description' => $this->weather->weather[0]->description,
+            'wind_speed' => $this->weather->wind->speed,
+            'wind_direction' => $this->weather->wind->deg,
+            'wind_direction_string' => $this->getWindDirection()
+        ];
+    }
 
-    public function __toString(): string {
+    public function __toString(): string
+    {
         if (is_null($this->weather)) {
             return 'Информация о погоде отсутствует';
         }
-        return $this->getTemperature().'('.$this->getTempFeelsLike().')&deg;C, '.$this->getHumidity().'%, '.$this->getPressure().'&nbsp;мм.рт.ст., '.$this->weather->weather[0]->description.', ветер '.$this->weather->wind->speed.' м/с, направление '.$this->getWindDirection().' ('.$this->weather->wind->deg.')';
+        return $this->getTemperature() . '(' . $this->getTempFeelsLike() . ')&deg;C, ' . $this->getHumidity() . '%, ' . $this->getPressure() . '&nbsp;мм.рт.ст., ' . $this->weather->weather[0]->description . ', ветер ' . $this->weather->wind->speed . ' м/с, направление ' . $this->getWindDirection() . ' (' . $this->weather->wind->deg . ')';
     }
 
-    public function getInitDataList(): array {
-        return ['api_key'=>'Ключ API', 'city_id'=>'ID города'];
+    public function getInitDataList(): array
+    {
+        return ['api_key' => 'Ключ API', 'city_id' => 'ID города'];
     }
 
-    public function getInitDataValues(): array {
-        return ['api_key'=>$this->api_key, 'city_id'=>$this->city_id];
+    public function getInitDataValues(): array
+    {
+        return ['api_key' => $this->api_key, 'city_id' => $this->city_id];
     }
 
-    public function getLastUpdate(): int {
+    public function getLastUpdate(): int
+    {
         return $this->updated;
     }
 
-    public function init($device_id, $init_data): void {
-        $this->hwid=$device_id;
-        foreach ($init_data as $key=> $value) {
-            $this->$key=$value;
+    public function init($device_id, $init_data): void
+    {
+        $this->hwid = $device_id;
+        foreach ($init_data as $key => $value) {
+            $this->$key = $value;
         }
-        if (is_null($this->api_key)) {
-            return;
-        }
-        if (is_null($this->city_id)) {
-            return;
-        }
-        $this->owm=new Api($this->api_key);
-        $this->owm->setCityId($this->city_id);
     }
 
-    public function update(): bool {
-        if(is_null($this->owm)) {
+    public function update(): bool
+    {
+        if (is_null($this->api_key)) {
             return false;
         }
-        $weather=$this->owm->fetchCurrent();
+        if (is_null($this->city_id)) {
+            return false;
+        }
+        $weather = $this->fetch();
         if (is_null($weather)) {
             return false;
         }
-        if ($this->updated==$weather->dt) {
-            return true;
+        if ($this->updated == $weather->dt) {
+            return false;
         }
-        $this->weather=$weather;
-        $this->updated=$weather->dt;
-        $storage=new DeviceStorage;
-        $storage->set($this->hwid, $this);
-        $events=['temperature'=>$weather->main->temp, 'humidity'=>$weather->main->humidity, 'pressure'=>round($weather->main->pressure*76000/101325, 2), 'wind_speed'=>$this->weather->wind->speed, 'wind_direction'=>$this->weather->wind->deg, 'weather'=>json_encode($weather, JSON_UNESCAPED_UNICODE)];
-        $uid=Devices::getUidByHwid($this->hwid);
-        Devices::processEvents($uid, $events, $weather->dt);
+        $this->weather = $weather;
+        $this->updated = $weather->dt;
+        $this->events = ['temperature' => $weather->main->temp, 'humidity' => $weather->main->humidity, 'pressure' => round($weather->main->pressure * 76000 / 101325, 2), 'wind_speed' => $this->weather->wind->speed, 'wind_direction' => $this->weather->wind->deg, 'weather' => json_encode($weather, JSON_UNESCAPED_UNICODE)];
         return true;
     }
 
-    public function getTemperature() {
-        return isset($this->weather->main->temp)?$this->weather->main->temp:null;
+    public function getEvents()
+    {
+        if (empty($this->events)) {
+            return null;
+        }
+        $events = $this->events;
+        $this->events = null;
+        return $events;
     }
 
-    public function getTempFeelsLike() {
-        return isset($this->weather->main->temp)?$this->weather->main->feels_like:null;
+    public function getTemperature()
+    {
+        return isset($this->weather->main->temp) ? $this->weather->main->temp : null;
     }
 
-    public function getHumidity() {
-        return isset($this->weather->main->humidity)?$this->weather->main->humidity:null;
+    public function getTempFeelsLike()
+    {
+        return isset($this->weather->main->temp) ? $this->weather->main->feels_like : null;
     }
 
-    public function getPressure() {
-        return isset($this->weather->main->pressure)?round($this->weather->main->pressure*76000/101325, 2):null;
+    public function getHumidity()
+    {
+        return isset($this->weather->main->humidity) ? $this->weather->main->humidity : null;
     }
 
-    public function getWindSpeed() {
+    public function getPressure()
+    {
+        return isset($this->weather->main->pressure) ? round($this->weather->main->pressure * 76000 / 101325, 2) : null;
+    }
+
+    public function getWindSpeed()
+    {
         if (!isset($this->weather->wind->speed)) {
             return '-';
         }
         return $this->weather->wind->speed;
     }
 
-    public function getWindDirection() {
+    public function getWindDirection()
+    {
         if (!isset($this->weather->wind->deg)) {
             return '-';
         }
-        $deg=$this->weather->wind->deg;
-        if ($deg<22) {
+        $deg = $this->weather->wind->deg;
+        if ($deg < 22) {
             return 'C';
-        } elseif ($deg<68) {
+        } elseif ($deg < 68) {
             return 'СЗ';
-        } elseif ($deg<112) {
+        } elseif ($deg < 112) {
             return 'З';
-        } elseif ($deg<158) {
+        } elseif ($deg < 158) {
             return 'ЮЗ';
-        } elseif ($deg<202) {
+        } elseif ($deg < 202) {
             return 'Ю';
-        } elseif ($deg<248) {
+        } elseif ($deg < 248) {
             return 'ЮВ';
-        } elseif ($deg<292) {
+        } elseif ($deg < 292) {
             return 'В';
-        } elseif ($deg<338) {
+        } elseif ($deg < 338) {
             return 'СВ';
         } else {
             return 'С';
         }
     }
 
+    public function getWeather() {
+        return $this->weather;
+    }
+
+    private function fetch()
+    {
+        $params['APPID'] = $this->api_key;
+        $params['id'] = $this->city_id;
+        $params['units'] = 'metric';
+        $params['lang'] = 'ru';
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, self::OWM_URL . 'weather?' . http_build_query($params));
+        curl_setopt($ch, CURLOPT_TIMEOUT, 5);
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 3);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+        $weather = curl_exec($ch);
+        curl_close($ch);
+        if ($weather === false) {
+            return null;
+        }
+        return json_decode($weather);
+    }
 }
---
+++ classes/PluginInfo.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace FSA\OpenWeatherPlugin;
+
+class PluginInfo
+{
+    
+    public function getDescription()
+    {
+        return 'Получение данных о погоде с сайта <a href="https://openweathermap.org">OpenWeather</a>.';
+    }
+}
